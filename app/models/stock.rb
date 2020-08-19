be74ae4fd618af45b67482f69243abd05686e728
Saad <khan.ssaad@gmail.com>
2020-08-19 22:11:36 +0530

    Add Ajax feature in portfolio page

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -6,6 +6,13 @@ class Stock < ApplicationRecord
         secret_token: Rails.application.credentials.iex_client[:sandbox_api_skey],
         endpoint: 'https://cloud.iexapis.com/v1'
     )
-    client.quote(ticker_symbol).latest_price
+    #client.quote(ticker_symbol)
+    #client.quote(ticker_symbol).latest_price
+    begin
+      new(ticker: ticker_symbol, name: client.quote(ticker_symbol).company_name, last_price: client.quote(ticker_symbol).latest_price)
+    rescue => exception
+      return nil
+    end
+
   end
 end
