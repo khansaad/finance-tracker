8049388e4d276d8a39afb8707149dc0954a6c46f
Saad <khan.ssaad@gmail.com>
2020-10-10 20:43:23 +0530

    Add user profile, their corresponding stocks view, etc

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
   get 'my_friends', to: 'users#my_friends'
   get 'search_friends', to: 'users#search_friends'
   resources :friendships, only: [:create, :destroy]
+  resources :users, only: [:show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
